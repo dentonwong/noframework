2db4db4439fc3bb2e205d33ed65e74569b20e9f5
Denton Wong <denton.wong@gmail.com>
2024-03-14 20:04:14 -0700

    removed .env and added to gitignore

--- server/models/ItemModel.js
+++ server/models/ItemModel.js
@@ -1,12 +1,6 @@
-// v-- REPLACE THE EMPTY STRING WITH YOUR LOCAL/MLAB/ELEPHANTSQL URI
 const {Pool} = require('pg');
 require('dotenv').config()
-const myURI = 'postgres://mcwtdzgz:cb445SehYKAJlvvafaQOU_E2DkDjLyrc@mahmud.db.elephantsql.com/mcwtdzgz';
 
-// UNCOMMENT THE LINE BELOW IF USING MONGO
-// const URI = process.env.MONGO_URI || myURI;
-
-// UNCOMMENT THE LINE BELOW IF USING POSTGRESQL
 const URI = process.env.PG_URI;
 
 const pool = new Pool({
@@ -25,4 +19,4 @@ db.query = (string, params, cb) => {
 db.query(setupStr);
 db.query(setupStr2);
 
-module.exports = db; // <-- export your model
+module.exports = db; 
